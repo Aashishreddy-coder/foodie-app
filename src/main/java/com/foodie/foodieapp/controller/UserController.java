9e4204c6fcea38a8ad9f62febd3cb246dd8a702c
Aashish <aashishreddythaduri@gmail.com>
2025-05-12 10:10:50 +0530

    small changes

--- src/main/java/com/foodie/foodieapp/controller/UserController.java
+++ src/main/java/com/foodie/foodieapp/controller/UserController.java
@@ -1,5 +1,20 @@
 package com.foodie.foodieapp.controller;
 
-public record UserController() {
-    
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.foodie.foodieapp.domain.AppUser;
+import com.foodie.foodieapp.service.UserService;
+
+@RestController
+@RequestMapping("/api/v1/users")
+public record UserController(UserService userService) {
+
+    @PostMapping
+    public ResponseEntity<AppUser> createUser(@RequestBody AppUser user) {
+        return ResponseEntity.ok(userService.createUser(user));
+    }
 }
