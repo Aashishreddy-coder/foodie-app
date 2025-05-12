3593bd6afff458607c392a7f345956f04355ba74
Aashish <aashishreddythaduri@gmail.com>
2025-05-12 09:52:15 +0530

    Added folder Structures ans some classes

---
+++ src/main/java/com/foodie/foodieapp/exceptions/CustomerNotFoundException.java
@@ -0,0 +1,9 @@
+package com.foodie.foodieapp.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(code = HttpStatus.NOT_FOUND , reason = "Customer not found")
+public class CustomerNotFoundException extends Exception {
+
+}
