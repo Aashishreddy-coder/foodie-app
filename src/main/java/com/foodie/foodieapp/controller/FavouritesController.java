5a5461535a49870fe2307a4ba8658d5860655133
bablu <saideepak.pokuri@gmail.com>
2025-05-16 11:46:48 +0530

    Added favourites section

---
+++ src/main/java/com/foodie/foodieapp/controller/FavouritesController.java
@@ -0,0 +1,33 @@
+package com.foodie.foodieapp.controller;
+
+import com.foodie.foodieapp.domain.Favourites;
+import com.foodie.foodieapp.service.FavouritesService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/favourites")
+public class FavouritesController {
+
+    @Autowired
+    private FavouritesService favouriteService;
+
+    @PostMapping("/save")
+    public ResponseEntity<?> saveDishes(@RequestBody Favourites favourites){
+        favouriteService.saveDishes(favourites);
+        return new ResponseEntity<>("Dish saved successfully",HttpStatus.CREATED);
+    }
+
+    @GetMapping("/get/{userId}")
+    public ResponseEntity<?> getAllDishes(@PathVariable Long userId){
+        return new ResponseEntity<>(favouriteService.getAllDishes(userId), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<?> deleteDishes(@PathVariable Long id){
+        favouriteService.deleteDishes(id);
+        return new ResponseEntity<>("Dish deleted successfully",HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/foodie/foodieapp/service/FavouritesService.java
@@ -0,0 +1,13 @@
+package com.foodie.foodieapp.service;
+
+import com.foodie.foodieapp.domain.Favourites;
+
+import java.util.List;
+
+public interface FavouritesService {
+    Favourites saveDishes(Favourites favourites);
+
+    void deleteDishes(Long id);
+
+    List<Favourites> getAllDishes(Long userId);
+}
